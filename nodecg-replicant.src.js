fb862ed43b7fc8c91eb2283c9420bb78c191f561
Alex Van Camp <email@alexvan.camp>
2016-03-31 10:24:22 -0500

    fix(binding): fix error thrown when splicing an array replicant on Polymer@^1.2.0

--- nodecg-replicant.src.js
+++ nodecg-replicant.src.js
@@ -62,16 +62,23 @@ Polymer({
                         this.notifyPath(path, change.newValue);
                         break;
                     case 'splice':
-                        /* Because Polymer is keeping its own internal copy of the Replicants value in
+                        /* Because Polymer is keeping its own internal copy of the Replicant's value in
                            a WeakMap, we have to alter the `removed` items of our splice record to point
                            to the items already in Polymer's store, otherwise it will fail to remove them. */
-                        var coll = Polymer._collections.get(this.value);
-                        var storeKeys = Object.keys(coll.store);
-                        for (var i = 0; i < change.removedCount; i++) {
-                            var removedItemIndexInStore = storeKeys[change.index];
-                            change.removed[i] = objectPath.get(coll.store, change.path)[removedItemIndexInStore];
-                        }
 
+                        // Lange 2016-03-31: this might not be necessary anymore. `coll` seems to be undefined
+                        // and this block throws an error, but things seem to still work?
+                        // I've wrapped it in an if statement for now.
+                        var coll = Polymer._collections.get(this.value);
+                        if (coll) {
+                            var storeKeys = Object.keys(coll.store);
+                            for (var i = 0; i < change.removedCount; i++) {
+                                var removedItemIndexInStore = storeKeys[change.index];
+                                change.removed[i] = objectPath.get(coll.store, change.path)[removedItemIndexInStore];
+                            }
+
+                        }
+                       
                         this.notifySplices(path, [change]);
                 }
             }.bind(this));
